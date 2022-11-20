d3c66208ba3f048916ffb00b50559901d1f478af
Alex <33667890+Felon@users.noreply.github.com>
2022-11-20 22:25:09 +0700

    Update test_items.py
    renamed function to lowercase

--- test_shop/test_items.py
+++ test_shop/test_items.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_Add_to_basket(browser):
+def test_guest_should_see_add_to_basket(browser):
     browser.get(link)
     button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert "successful" not in button.text
